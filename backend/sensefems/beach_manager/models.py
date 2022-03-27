afd9e0a398b38f5e5cf4c11d5e23dd55200bf843
Enrique <enrique.ferrer@espossible.com>
2022-03-27 17:29:01 +0200

    update model and import beaches

--- backend/sensefems/beach_manager/models.py
+++ backend/sensefems/beach_manager/models.py
@@ -3,7 +3,7 @@ from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 class Beach(models.Model):
-    name = models.CharField(unique=True, max_length=255)
+    name = models.CharField(max_length=255)
     municipality = models.CharField(max_length=255) 
 
     class Meta:
