e9659b242a5fb5a6188708c767bf93f1a7c40282
Enrique <enrique.ferrer@espossible.com>
2022-03-27 16:13:49 +0200

    add installed apps

--- backend/sensefems/sensefems/settings/base.py
+++ backend/sensefems/sensefems/settings/base.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+
+        # Discamino Apps #
+    'beach_manager.apps.BeachManagerConfig',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +128,13 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )    
+}
