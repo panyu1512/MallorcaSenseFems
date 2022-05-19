f03081d81ec219918cf2813feea1f0655649441a
Enrique <enrique.ferrer@espossible.com>
2022-05-20 00:18:16 +0200

    Add new component and delete the old one.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
